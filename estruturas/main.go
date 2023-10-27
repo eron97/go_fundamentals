122c1f7adf1699a29f32eedb874eb01628f3f422
eron97 <eronsantosbetine@gmail.com>
2023-10-27 15:44:52 -0300

    Create interfaces

--- estruturas/main.go
+++ estruturas/main.go
@@ -1,3 +1,23 @@
 package main
 
-func main() {}
+import "fmt"
+
+func main() {
+
+	// Criação de uma struct
+	type Person struct {
+		age  int
+		name string
+	}
+
+	// Criação de uma instância da struct
+	var person Person
+
+	// Atribuição de valores
+	person.age = 30
+	person.name = "John Doce"
+
+	fmt.Printf("Name: %s with age: %d", person.name, person.age)
+	// saída -> Name: John Doce with age: 30
+
+}
