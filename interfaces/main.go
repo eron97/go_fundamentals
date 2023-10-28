f0197977eae603281fcbe5c6b68b976b3d32f972
eron97 <eronsantosbetine@gmail.com>
2023-10-28 09:52:17 -0300

    interfaces ok

--- interfaces/main.go
+++ interfaces/main.go
@@ -5,22 +5,48 @@ import (
 	"math"
 )
 
-type Abser interface {
-	Abs() float64
+// Definindo a interface Shape
+type Shape interface {
+	Area() float64
 }
 
-type Point struct {
-	X, Y float64
+// Definindo a estrutura Circle
+type Circle struct {
+	Radius float64
 }
 
-func (p Point) Abs() float64 {
-	return math.Sqrt(p.X*p.X + p.Y*p.Y)
+// Implementação do método Area para Circle
+// Com Função acompanhado de receptor (receiver function):
+func (c Circle) Area() float64 {
+	return math.Pi * c.Radius * c.Radius
 }
 
-var p = Point{X: 3, Y: 4}
+// Definindo a estrutura Rectangle
+type Rectangle struct {
+	Width  float64
+	Height float64
+}
+
+// Implementação do método Area para Rectangle
+// Com Função sem receptor (receiver function):
+func Area(r Rectangle) float64 {
+	return r.Width * r.Height
+}
 
 func main() {
-	a := Abser(p)
-	fmt.Println(a.Abs())
+
+	// Criando uma instância de Circle
+	circle := Circle{Radius: 5}
+
+	// Criando uma instância de Rectangle
+	rectangle := Rectangle{Width: 4, Height: 6}
+
+	// Calculando a área do círculo usando a função name_function
+
+	fmt.Printf("Área do círculo: %.2f\n", circle.Area())
+	// saída -> Área do círculo: 78.54
+
+	fmt.Printf("Área do retângulo: %.2f\n", Area(rectangle))
+	// saída -> Área do retângulo: 24.00
 
 }
